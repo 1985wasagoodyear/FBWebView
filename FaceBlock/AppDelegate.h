d05451fddbea78fed917fc0959e6ff3e04613d16
1985wasagoodyear <1985wasagoodyear>
2019-09-13 08:33:39 -0400

    Initial Commit

---
+++ FaceBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceBlock
+//
+//  Created by K Y on 9/13/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceBlock
+//
+//  Created by K Y on 9/13/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
